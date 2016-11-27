6720f4058c49f7c88eb0b9b020363e7bbea5e16b
brodan <christopher.hranj@gmail.com>
2016-11-27 00:02:11 -0500

    updated to copy/rename/download letter.

--- coverlover.js
+++ coverlover.js
@@ -7,10 +7,11 @@ var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/script-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/drive'];
+var SCOPES = ['https://www.googleapis.com/auth/documents',
+              'https://www.googleapis.com/auth/drive'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'script-nodejs-quickstart.json';
+var TOKEN_PATH = TOKEN_DIR + 'coverLover.json';
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
@@ -20,7 +21,7 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   }
   // Authorize a client with the loaded credentials, then call the
   // Google Apps Script Execution API.
-  authorize(JSON.parse(content), callAppsScript);
+  authorize(JSON.parse(content), getDoc);
 });
 
 /**
@@ -97,21 +98,63 @@ function storeToken(token) {
   console.log('Token stored to ' + TOKEN_PATH);
 }
 
+function getDoc(auth) {
+  var service = google.drive('v3');
+  service.files.list({
+    auth: auth,
+    pageSize: 1,
+    q: "name='Generic Cover Letter'",
+    fields: 'files(id)'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var files = response.files;
+    if (files.length == 0) {
+      console.log('No files found.');
+    } else {
+      //console.log(response.files[0].id);
+      createDoc(auth, response.files[0].id)
+    }
+  });
+}
+
+function createDoc(auth, originalDocID){
+  var service = google.drive('v3');
+  service.files.copy({
+    auth: auth,
+    fileId: originalDocID
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    callAppsScript(auth, response.id)
+  });
+}
+
 /**
  * Call an Apps Script function to list the folders in the user's root
  * Drive folder.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function callAppsScript(auth) {
-  var scriptId = 'ENTER_YOUR_SCRIPT_ID_HERE';
+function callAppsScript(auth, copiedDocID) {
+  var scriptId = process.env.GOOGLE_SCRIPT_ID;
   var script = google.script('v1');
 
   // Make the API request. The request object is included here as 'resource'.
   script.scripts.run({
     auth: auth,
     resource: {
-      function: 'getFoldersUnderRoot'
+      function: 'findAndReplace',
+      parameters: [
+        documentID=copiedDocID,
+        companyName="TestCompany",
+        companyPosition="TestPosition"
+      ],
+      devMode: true
     },
     scriptId: scriptId
   }, function(err, resp) {
@@ -126,11 +169,11 @@ function callAppsScript(auth) {
       // Extract the first (and only) set of error details. The values of this
       // object are the script's 'errorMessage' and 'errorType', and an array
       // of stack trace elements.
+      console.log(resp.error)
       var error = resp.error.details[0];
-      console.log('Script error message: ' + error.errorMessage);
-      console.log('Script error stacktrace:');
-
+      //console.log('Script error message: ' + error.errorMessage);
       if (error.scriptStackTraceElements) {
+        console.log('Script error stacktrace:');
         // There may not be a stacktrace if the script didn't start executing.
         for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
           var trace = error.scriptStackTraceElements[i];
@@ -138,20 +181,26 @@ function callAppsScript(auth) {
         }
       }
     } else {
-      // The structure of the result will depend upon what the Apps Script
-      // function returns. Here, the function returns an Apps Script Object
-      // with String keys and values, and so the result is treated as a
-      // Node.js object (folderSet).
-      var folderSet = resp.response.result;
-      if (Object.keys(folderSet).length == 0) {
-        console.log('No folders returned!');
-      } else {
-        console.log('Folders under your root folder:');
-        Object.keys(folderSet).forEach(function(id){
-          console.log('\t%s (%s)', folderSet[id], id);
-        });
-      }
+      
+      //console.log(resp);
+      downloadLetter(auth, copiedDocID);
     }
-
   });
+}
+
+function downloadLetter(auth, fileID){
+  var dest = fs.createWriteStream('coverLetter.pdf'); //TODO: Dynamic PDF name. 
+  var service = google.drive('v3');
+  service.files.export({
+    auth: auth,
+    fileId: fileID,
+    mimeType: 'application/pdf'
+  })
+  .on('end', function() {
+    console.log('Download Successful.');
+  })
+  .on('error', function(err) {
+    console.log('Error during download', err);
+  })
+  .pipe(dest);
 }
