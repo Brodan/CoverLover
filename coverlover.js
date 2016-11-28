d7df1f2641b27d8ca23810ca71db49fd09688b9f
brodan <christopher.hranj@gmail.com>
2016-11-27 22:55:05 -0500

    update to use promises

--- coverlover.js
+++ coverlover.js
@@ -1,28 +1,36 @@
-// source: https://developers.google.com/apps-script/guides/rest/quickstart/nodejs
-
 var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/script-nodejs-quickstart.json
+// at ~/.credentials/coverLover.json
 var SCOPES = ['https://www.googleapis.com/auth/documents',
               'https://www.googleapis.com/auth/drive'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'coverLover.json';
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Apps Script Execution API.
-  authorize(JSON.parse(content), getDoc);
-});
+readSecrets()
+  .then(authorize)
+  .then(getDoc)
+  .then(createDoc)
+  .then(callAppsScript)
+  .then(downloadLetter);
+
+function readSecrets(){
+  var promise = new Promise(function(resolve, reject){
+    fs.readFile('client_secret.json', function (err, content) {
+      if (err) {
+        reject(Error("It broke readSecrets()"));
+      }
+      else {
+        resolve(JSON.parse(content));
+      }
+    });
+  });
+  return promise;
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -31,22 +39,25 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
   var redirectUrl = credentials.installed.redirect_uris[0];
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
+  var promise = new Promise(function(resolve, reject){
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+      if (err) {
+        getNewToken(oauth2Client, callback);
+      } else {
+        oauth2Client.credentials = JSON.parse(token);
+        resolve(oauth2Client);
+      }
+    });
   });
+  return promise;
 }
 
 /**
@@ -100,100 +111,110 @@ function storeToken(token) {
 
 function getDoc(auth) {
   var service = google.drive('v3');
-  service.files.list({
-    auth: auth,
-    pageSize: 1,
-    q: "name='Generic Cover Letter'",
-    fields: 'files(id)'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var files = response.files;
-    if (files.length == 0) {
-      console.log('No files found.');
-    } else {
-      //console.log(response.files[0].id);
-      createDoc(auth, response.files[0].id)
-    }
+  var promise = new Promise(function(resolve, reject){
+    service.files.list({
+      auth: auth,
+      pageSize: 1,
+      q: "name='Generic Cover Letter'",
+      fields: 'files(id)'
+    }, function(err, response) {
+      if (err) {
+        console.log("HERE2")
+        reject(Error("It broke in getDoc()"));
+      }
+      else{
+        var files = response.files;
+        if (files.length == 0) {
+          console.log('No files found.');
+        } else {
+          resolve([auth, response.files[0].id]);
+        }
+      }
+    });
   });
+  return promise;
 }
 
-function createDoc(auth, originalDocID){
+function createDoc(args){
   var service = google.drive('v3');
-  service.files.copy({
-    auth: auth,
-    fileId: originalDocID
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    callAppsScript(auth, response.id)
+  var promise = new Promise(function(resolve, reject){
+    service.files.copy({
+      auth: args[0],
+      fileId: args[1]
+    }, function(err, response) {
+      if (err) {
+        console.log(err);
+        reject(Error("It broke in createDoc()"));
+      }
+      else{
+        resolve([args[0], response.id]);
+      }
+    });
   });
+  return promise;
 }
 
 /**
- * Call an Apps Script function to list the folders in the user's root
- * Drive folder.
+ * Call an Apps Script function to replace company name and position in cover letter.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function callAppsScript(auth, copiedDocID) {
+function callAppsScript(args) {
   var scriptId = process.env.GOOGLE_SCRIPT_ID;
   var script = google.script('v1');
 
-  // Make the API request. The request object is included here as 'resource'.
-  script.scripts.run({
-    auth: auth,
-    resource: {
-      function: 'findAndReplace',
-      parameters: [
-        documentID=copiedDocID,
-        companyName="TestCompany",
-        companyPosition="TestPosition"
-      ],
-      devMode: true
-    },
-    scriptId: scriptId
-  }, function(err, resp) {
-    if (err) {
-      // The API encountered a problem before the script started executing.
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    if (resp.error) {
-      // The API executed, but the script returned an error.
-
-      // Extract the first (and only) set of error details. The values of this
-      // object are the script's 'errorMessage' and 'errorType', and an array
-      // of stack trace elements.
-      console.log(resp.error)
-      var error = resp.error.details[0];
-      //console.log('Script error message: ' + error.errorMessage);
-      if (error.scriptStackTraceElements) {
-        console.log('Script error stacktrace:');
-        // There may not be a stacktrace if the script didn't start executing.
-        for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-          var trace = error.scriptStackTraceElements[i];
-          console.log('\t%s: %s', trace.function, trace.lineNumber);
-        }
+  var promise = new Promise(function(resolve, reject){
+    // Make the API request. The request object is included here as 'resource'.
+    script.scripts.run({
+      auth: args[0],
+      resource: {
+        function: 'findAndReplace',
+        parameters: [
+          documentID=args[1],
+          companyName="TestCompany",
+          companyPosition="TestPosition"
+        ],
+        devMode: true
+      },
+      scriptId: scriptId
+    }, function(err, resp) {
+      if (err) {
+        // The API encountered a problem before the script started executing.
+        console.log('The API returned an error: ' + err);
+        reject(Error("It broke in callAppsScript()"));
       }
-    } else {
-      
-      //console.log(resp);
-      downloadLetter(auth, copiedDocID);
-    }
+      if (resp.error) {
+        // The API executed, but the script returned an error.
+
+        // Extract the first (and only) set of error details. The values of this
+        // object are the script's 'errorMessage' and 'errorType', and an array
+        // of stack trace elements.
+        console.log(resp.error)
+        var error = resp.error.details[0];
+        //console.log('Script error message: ' + error.errorMessage);
+        if (error.scriptStackTraceElements) {
+          console.log('Script error stacktrace:');
+          // There may not be a stacktrace if the script didn't start executing.
+          for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+            var trace = error.scriptStackTraceElements[i];
+            console.log('\t%s: %s', trace.function, trace.lineNumber);
+          }
+        }
+        eject(Error("It broke in callAppsScript()"));
+      } else {
+        resolve([args[0], args[1]])
+      }
+    });
   });
+  return promise;
 }
 
-function downloadLetter(auth, fileID){
+function downloadLetter(args){
   var dest = fs.createWriteStream('coverLetter.pdf'); //TODO: Dynamic PDF name. 
   var service = google.drive('v3');
   service.files.export({
-    auth: auth,
-    fileId: fileID,
+    auth: args[0],
+    fileId: args[1],
     mimeType: 'application/pdf'
   })
   .on('end', function() {
